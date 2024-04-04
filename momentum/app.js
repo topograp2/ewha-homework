b26f697422e7663b290bf2598876266532b1edb2
topograp2 <topo.or.dubu@gmail.com>
2024-04-04 21:37:46 +0900

    lecture 4 end

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,32 @@
-const $hii = document.querySelector(".hello .mii");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function changeColor(){
-    
-    $hii.classList.toggle("active");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(loginInput.value);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-$hii.addEventListener("click", changeColor);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME);
+
+if(savedUsername === null){ // username 아직 submit 안함
+    //show form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+}else{
+    paintGreetings(savedUsername);
+}
