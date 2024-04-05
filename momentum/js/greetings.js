93830f80aec7aa5edd00cc183d4dd35bc1cc2f94
topograp2 <topo.or.dubu@gmail.com>
2024-04-05 20:37:40 +0900

    lecture 6 end

--- momentum/js/greetings.js
+++ momentum/js/greetings.js
@@ -2,6 +2,7 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME = "username";
 
